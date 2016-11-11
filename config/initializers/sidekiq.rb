59691d4a8f2e925d10241fdc21f320811fb4f41e
kelishrestha <kylesth@gmail.com>
2016-11-11 20:54:59 +0545

    Refactor code.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,11 +4,19 @@ require 'sidekiq/web'
 path = File.expand_path('../../redis.yml', __FILE__)
 redis_config = YAML.load(ERB.new(File.read(path)).result)[(ENV['RACK_ENV'] || 'development')]
 sidekiq_db = redis_config['db'] || 0
+redis_port = redis_config['port']
+redis_host = redis_config['host']
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{redis_config['host']}:#{redis_config['port']}/#{redis_config['db']}", namespace: 'sidekiq' }
+  config.redis = {
+    url: "redis://#{redis_host}:#{redis_port}/#{redis_config['db']}",
+    namespace: 'sidekiq'
+  }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{redis_config['sidekiq_host'] || redis_config['host']}:#{redis_config['port']}/#{sidekiq_db}", namespace: 'sidekiq', size: 1 }
+  config.redis = {
+    url: "redis://#{redis_config['sidekiq_host'] || redis_host}:#{redis_port}/#{sidekiq_db}",
+    namespace: 'sidekiq', size: 1
+  }
 end
